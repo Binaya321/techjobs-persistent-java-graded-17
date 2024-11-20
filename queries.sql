1fdc3a40acb6c75ce10ecbfec11da78b42442d98
binayapaudel <binayapaudel782@gmail.com>
2024-11-19 23:08:02 -0500

    GA#4 complete

--- queries.sql
+++ queries.sql
@@ -1,7 +1,18 @@
 --Part 1
 
+Field	Type	Null	Key	Default	Extra
+id	int	NO	PRI	NULL
+employer	varchar(255)	YES		NULL
+name	varchar(255)	YES		NULL
+skills	varchar(255)	YES		NULL
+
 --Part 2
-
+SELECT name FROM employer WHERE location = "St. Louis City";
 --Part 3
-
+DROP TABLE job;
 --Part 4
+SELECT *
+FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,32 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private final List<Job> jobs = new ArrayList<>();
+
+    @NotBlank(message = "Location can't be blank!")
+    @Size(min = 3, max = 100)
+    private String location;
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
 }
